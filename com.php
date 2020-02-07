055e145a33924c093b3103c10596124c98a5634a
MeidenWeiss <60122610+MeidenWeiss@users.noreply.github.com>
2020-02-07 14:58:28 +0800

    Add files via upload

---
+++ com.php
@@ -0,0 +1,26 @@
+<?php
+    header("Content-Type: application/rss+xml; charset=ISO-8859-1");
+    $rssfeed = '<?xml version="1.0" encoding="ISO-8859-1"?>';
+    $rssfeed .= '<rss version="2.0">';
+    $rssfeed .= '<channel>';
+    
+    $sql = "SELECT * FROM tbl_aero ORDER BY aero_id ASC;";
+    $connect = mysqli_connect("localhost", "root", "") or die(mysqli_error($connect));
+	$db = mysqli_select_db($connect, "test");
+    $query = mysqli_query($connect,$sql) or die(mysqli_error($connect));
+    while($row=mysqli_fetch_assoc($query)) {
+        extract($row);
+
+        $rssfeed .= '<item>';
+        $rssfeed .= '<id>' . $aero_id . '</id>';
+        $rssfeed .= '<title>' . $aero_name . '</title>';
+        $rssfeed .= '<web>' . $aero_web . '</web>';
+        $rssfeed .= '<rss>' . $aero_rss . '</rss>';
+        $rssfeed .= '</item>';
+    }
+ 
+    $rssfeed .= '</channel>';
+    $rssfeed .= '</rss>';
+
+    echo $rssfeed;
+?>
