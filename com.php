a89afe7a846eb7e80afa08bda6f682cbd174e1d9
MeidenWeiss <60122610+MeidenWeiss@users.noreply.github.com>
2020-02-07 15:52:38 +0800

    Update com.php

--- com.php
+++ com.php
@@ -6,7 +6,7 @@
     
     $sql = "SELECT * FROM tbl_aero ORDER BY aero_id ASC;";
     $connect = mysqli_connect("us-cdbr-iron-east-04.cleardb.net", "bd6741228486af", "11e0ed1a") or die(mysqli_error($connect));
-	$db = mysqli_select_db($connect, "test");
+	$db = mysqli_select_db($connect, "heroku_22e4df063db1a40");
     $query = mysqli_query($connect,$sql) or die(mysqli_error($connect));
     while($row=mysqli_fetch_assoc($query)) {
         extract($row);
